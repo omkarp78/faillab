d99c0dca0649d8f24512428a4b4edb054531a13e
omkarp78 <omkarpatil0806@gmail.com>
2026-09-16 16:21:54 +0530

    Phase 5: activate UI localization bridge

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {LocalizationBridge} from './LocalizationBridge';
 import {PreferencesBar,PreferencesProvider} from './preferences';
 import './styles.css';
 import './preferences.css';
@@ -8,6 +9,7 @@ import './preferences.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
  <React.StrictMode>
   <PreferencesProvider>
+   <LocalizationBridge/>
    <PreferencesBar/>
    <App/>
   </PreferencesProvider>
