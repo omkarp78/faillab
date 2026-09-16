d82f4686b1b89fef942404fbbe9e79ef18edbad1
omkarp78 <omkarpatil0806@gmail.com>
2026-09-16 16:14:53 +0530

    Phase 5: mount personalization controls

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {PreferencesBar,PreferencesProvider} from './preferences';
 import './styles.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+ <React.StrictMode>
+  <PreferencesProvider>
+   <PreferencesBar/>
+   <App/>
+  </PreferencesProvider>
+ </React.StrictMode>
+);
