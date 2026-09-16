3185a89ed40598c493b702f967ce73628eded9c7
omkarp78 <omkarpatil0806@gmail.com>
2026-09-16 16:15:19 +0530

    Phase 5: load personalization styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {PreferencesBar,PreferencesProvider} from './preferences';
 import './styles.css';
+import './preferences.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
  <React.StrictMode>
